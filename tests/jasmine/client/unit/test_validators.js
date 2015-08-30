28a077b339afdd5ceab2fb77f05d1ca6cf8c93ec
Marvin Arnold <marvinmarnold@gmail.com>
2015-08-30 17:56:41 -0500

    First commit. Email validation test

---
+++ tests/jasmine/client/unit/test_validators.js
@@ -0,0 +1,9 @@
+describe("Email validation", function() {
+  it("works for some common emails", function() {
+    var emails = ["marvin.m.arnold@gmail.com", "marvin@unplugged.im", "marvin@ostel.co"]
+    for(var i = 0; i < emails.length; i++) {
+      var email = emails[i];
+      expect(isValidEmail(email)).toEqual(true);
+    }
+  });
+});
