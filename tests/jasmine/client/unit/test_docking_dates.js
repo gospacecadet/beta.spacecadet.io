7221376b53d413ddc8b7069ef2c5924bd34aea7c
Marvin Arnold <marvinmarnold@gmail.com>
2015-09-02 22:13:17 -0500

    Test that start and end date default to today

--- tests/jasmine/client/unit/test_docking_dates.js
+++ tests/jasmine/client/unit/test_docking_dates.js
@@ -1,51 +1,50 @@
-// var customMatchers = {
-//   toBeJustBefore: function(util, customEqualityTesters) {
-//     var delayTolerance = 0; // miliseconds
-//     return {
-//       compare: function(actual, expected) {
-//         if (expected === undefined) {
-//           expected = '';
-//         }
+var customMatchers = {
+  toBeJustBefore: function(util, customEqualityTesters) {
+    var delayTolerance = 3; // miliseconds
+    return {
+      compare: function(actual, expected) {
+        if (expected === undefined) {
+          expected = '';
+        }
 
-//         var result = {};
-//         result.pass = expected < actual + delayTolerance;
+        var result = {};
+        result.pass = expected < actual + delayTolerance;
 
-//         if(result.pass) {
-//           result.message = "Expected " + actual + " not to be within " + delayTolerance + " miliseconds of " + expected;
-//         } else {
-//           result.message = "Expected " + actual + " to be within " + delayTolerance + " miliseconds of " + expected;
-//         }
+        if(result.pass) {
+          result.message = "Expected " + actual + " not to be within " + delayTolerance + " miliseconds of " + expected;
+        } else {
+          result.message = "Expected " + actual + " to be within " + delayTolerance + " miliseconds of " + expected;
+        }
 
-//         return result;
-//       }
-//     }
-//   }
-// }
+        return result;
+      }
+    }
+  }
+}
 
 describe("The dates selected for the current docking", function() {
   // Reset the session variables
-  // beforeEach(function() {
-    // jasmine.addMatchers(customMatchers);
-    // Session.set(SESSION_LOOKUP_START_DOCKING_ON, undefined);
-    // Session.set(SESSION_LOOKUP_END_DOCKING_ON, undefined);
-  // });
+  beforeEach(function() {
+    jasmine.addMatchers(customMatchers);
+    Session.set(SESSION_LOOKUP_START_DOCKING_ON, undefined);
+    Session.set(SESSION_LOOKUP_END_DOCKING_ON, undefined);
+  });
 
-  // it("start date defaults to todays date", function() {
-  //   expectDefaultDate(startDockingOn().getTime());
-  // });
+  it("start date defaults to todays date", function() {
+    expectDefaultDate(startDockingOn().getTime());
+  });
 
-  // it("end date defaults to todays date", function() {
-  //   expectDefaultDate(endDockingOn().getTime());
-  // });
+  it("end date defaults to todays date", function() {
+    expectDefaultDate(endDockingOn().getTime());
+  });
 
   it("always knows what today is", function() {
-    // var today = today().getTime();
-    // var now = (new Date()).getTime();
-    // expect(today).toBeJustBefore(now);
-    expect(true).toBe(false);
-  }
+    var _today = today().getTime();
+    var _now = (new Date()).getTime();
+    expect(_today).toBeJustBefore(_now);
+  });
 
-  // var expectDefaultDate = function(date) {
-  //   expect(date).toBeJustBefore(today().getTime());
-  // }
+  var expectDefaultDate = function(date) {
+    expect(date).toBeJustBefore(today().getTime());
+  }
 });
--- tests/jasmine/client/unit/test_validators.js
+++ tests/jasmine/client/unit/test_validators.js
@@ -3,8 +3,7 @@ describe("Email validation", function() {
     var emails = ["marvin.m.arnold@gmail.com", "marvin@unplugged.im", "marvin@ostel.co"]
     for(var i = 0; i < emails.length; i++) {
       var email = emails[i];
-      // expect(isValidEmail(email)).toEqual(true);
-      expect(true).toBe(false);
+      expect(isValidEmail(email)).toEqual(true);
     }
   });
 });
