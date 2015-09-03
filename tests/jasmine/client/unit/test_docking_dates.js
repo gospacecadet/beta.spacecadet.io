18a59488c43f81220d159b1e201cc5f707fafdbc
Marvin Arnold <marvinmarnold@gmail.com>
2015-09-02 21:54:05 -0500

    broke tests

--- tests/jasmine/client/unit/test_docking_dates.js
+++ tests/jasmine/client/unit/test_docking_dates.js
@@ -1,49 +1,51 @@
-var customMatchers = {
-  toBeJustBefore: function(util, customEqualityTesters) {
-    var delayTolerance = 3; // miliseconds
-    return {
-      compare: function(actual, expected) {
-        if (expected === undefined) {
-          expected = '';
-        }
+// var customMatchers = {
+//   toBeJustBefore: function(util, customEqualityTesters) {
+//     var delayTolerance = 0; // miliseconds
+//     return {
+//       compare: function(actual, expected) {
+//         if (expected === undefined) {
+//           expected = '';
+//         }
 
-        var result = {};
-        result.pass = expected < actual + delayTolerance;
+//         var result = {};
+//         result.pass = expected < actual + delayTolerance;
 
-        if(result.pass) {
-          result.message = "Expected " + actual + " not to be within " + delayTolerance + " miliseconds of " + expected;
-        } else {
-          result.message = "Expected " + actual + " to be within " + delayTolerance + " miliseconds of " + expected;
-        }
+//         if(result.pass) {
+//           result.message = "Expected " + actual + " not to be within " + delayTolerance + " miliseconds of " + expected;
+//         } else {
+//           result.message = "Expected " + actual + " to be within " + delayTolerance + " miliseconds of " + expected;
+//         }
 
-        return result;
-      }
-    }
-  }
-}
-
+//         return result;
+//       }
+//     }
+//   }
+// }
 
 describe("The dates selected for the current docking", function() {
-  var timeTolerance = 5;
-
   // Reset the session variables
-  beforeEach(function() {
-    jasmine.addMatchers(customMatchers);
-    Session.set(SESSION_LOOKUP_START_DOCKING_ON, undefined);
-    Session.set(SESSION_LOOKUP_END_DOCKING_ON, undefined);
-  });
-
-  // it("start date defaults to todays date", function() {
-  //   expect(startDockingOn().getTime()).toEqual(today().getTime());
+  // beforeEach(function() {
+    // jasmine.addMatchers(customMatchers);
+    // Session.set(SESSION_LOOKUP_START_DOCKING_ON, undefined);
+    // Session.set(SESSION_LOOKUP_END_DOCKING_ON, undefined);
   // });
 
-  it("end date defaults to todays date", function() {
-    var tolerance = 5;
-    expect(endDockingOn().getTime()).toBeJustBefore(today().getTime());
-    // Don't need to check greater than since left side is evaluated first
-  });
+  // it("start date defaults to todays date", function() {
+  //   expectDefaultDate(startDockingOn().getTime());
+  // });
 
-  // it("always knows what today is"), function() {
-  //   expect(today().getTime()).toEqual((new Date()).getTime();
+  // it("end date defaults to todays date", function() {
+  //   expectDefaultDate(endDockingOn().getTime());
+  // });
+
+  it("always knows what today is", function() {
+    // var today = today().getTime();
+    // var now = (new Date()).getTime();
+    // expect(today).toBeJustBefore(now);
+    expect(true).toBe(false);
+  }
+
+  // var expectDefaultDate = function(date) {
+  //   expect(date).toBeJustBefore(today().getTime());
   // }
 });
