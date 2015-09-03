26a5bffd260a1605282f72f55031450befac8f54
Marvin Arnold <marvinmarnold@gmail.com>
2015-09-02 22:30:05 -0500

    Refactored tests into smaller contexts

--- tests/jasmine/client/unit/test_docking_dates.js
+++ tests/jasmine/client/unit/test_docking_dates.js
@@ -22,7 +22,37 @@ var customMatchers = {
   }
 }
 
-describe("The dates selected for the current docking", function() {
+describe("The current docking", function() {
+  describe('set dates', function() {
+    var _randomDate;
+
+    beforeEach(function() {
+      var _day = Math.floor((Math.random() * 28) + 1);
+      var _month = Math.floor((Math.random() * 12) + 1);
+      var _year = Math.floor((Math.random() * 2015) + 1);
+      _randomDate = new Date(_year, _month, _day);
+    });
+
+    it("can have its start date set", function() {
+      setStartDockingOn(_randomDate);
+      expect(getStartDockingOn()).toEqual(_randomDate);
+    });
+
+    it("can have its end date set", function() {
+      setEndDockingOn(_randomDate);
+      expect(getEndDockingOn()).toEqual(_randomDate);
+    });
+
+    it("always knows what today is", function() {
+      jasmine.addMatchers(customMatchers);
+      var _today = today().getTime();
+      var _now = (new Date()).getTime();
+      expect(_today).toBeJustBefore(_now);
+    });
+  });
+});
+
+describe("The current docking is undefined", function() {
   // Reset the session variables
   beforeEach(function() {
     jasmine.addMatchers(customMatchers);
@@ -31,17 +61,11 @@ describe("The dates selected for the current docking", function() {
   });
 
   it("start date defaults to todays date", function() {
-    expectDefaultDate(startDockingOn().getTime());
+    expectDefaultDate(getStartDockingOn().getTime());
   });
 
   it("end date defaults to todays date", function() {
-    expectDefaultDate(endDockingOn().getTime());
-  });
-
-  it("always knows what today is", function() {
-    var _today = today().getTime();
-    var _now = (new Date()).getTime();
-    expect(_today).toBeJustBefore(_now);
+    expectDefaultDate(getEndDockingOn().getTime());
   });
 
   var expectDefaultDate = function(date) {
