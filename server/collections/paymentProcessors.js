6d62ba3a4ba1bab4c231e82a9bd8c8d413072c88
Marvin Arnold <marvinmarnold@gmail.com>
2015-11-14 18:18:45 -0500

    details submitted, charges enabled, and transfers enabled from stripe

--- server/collections/paymentProcessors.js
+++ server/collections/paymentProcessors.js
@@ -5,18 +5,24 @@ Meteor.methods({
     var retrieveAccount = Meteor.wrapAsync(Stripe.accounts.retrieve, Stripe.accounts);
 
     try {
+      var paymentProcessor = PaymentProcessors.findOne({processorName: "Stripe"});
       var result = retrieveAccount();
-      paymentProcessor = {
+      // var external_account = result.external_accounts.data
+      console.log(result);
+      var paymentProcessorAttrs = {
         processorName: "Stripe",
         businessName: result.business_name,
         businessURL: result.business_url,
         detailsSubmitted: result.details_submitted,
-        // chargesEnabled: result.charges_enabled,
-        // transfersEnabled: result.transfers_enabled,
-        // transferSchedule: result.transfer_schedule.interval
+        chargesEnabled: result.charge_enabled,
+        transfersEnabled: result.transfer_enabled
       }
 
-      PaymentProcessors.insert(paymentProcessor);
+      if(paymentProcessor) {
+        PaymentProcessors.update(paymentProcessor._id, { $set: paymentProcessorAttrs })
+      } else {
+        PaymentProcessors.insert(paymentProcessorAttrs);
+      }
     } catch (error) {
       throw new Meteor.Error("stripe-charge-error", error.message);
     }
