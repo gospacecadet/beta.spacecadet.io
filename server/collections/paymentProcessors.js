11cc066b7e0c8e646d177b3df81fa4c0ea061ac4
Marvin Arnold <marvinmarnold@gmail.com>
2015-11-14 17:36:38 -0500

    Fetch relevant account wide details from stripe

--- server/collections/paymentProcessors.js
+++ server/collections/paymentProcessors.js
@@ -1,5 +1,24 @@
 Meteor.methods({
   updatePaymentProcessors: function() {
+    var stripeAPIKey = AdminSettings.findOne({"key": "stripeAPIKey"})
+    Stripe = StripeAPI(stripeAPIKey.value);
+    var retrieveAccount = Meteor.wrapAsync(Stripe.accounts.retrieve, Stripe.accounts);
 
+    try {
+      var result = retrieveAccount();
+      paymentProcessor = {
+        processorName: "Stripe",
+        businessName: result.business_name,
+        businessURL: result.business_url,
+        detailsSubmitted: result.details_submitted,
+        // chargesEnabled: result.charges_enabled,
+        // transfersEnabled: result.transfers_enabled,
+        // transferSchedule: result.transfer_schedule.interval
+      }
+
+      PaymentProcessors.insert(paymentProcessor);
+    } catch (error) {
+      throw new Meteor.Error("stripe-charge-error", error.message);
+    }
   }
 });
