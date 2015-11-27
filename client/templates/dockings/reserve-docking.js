00e86a0e1eef216833221a33d9548c2e1befa4ab
Marvin Arnold <marvinmarnold@gmail.com>
2015-11-27 16:48:44 -0600

    Reactive line items & can remove line items from docking

--- client/templates/dockings/reserve-docking.js
+++ client/templates/dockings/reserve-docking.js
@@ -1,17 +1,5 @@
-Template.reserveDocking.helpers({
-  stationName: function() {
-    var lp = Mart.Products.findOne(this.productId)
-    if(lp) {
-      var storefrontId = lp.storefrontId;
-      return Mart.Storefronts.findOne(storefrontId).name
-    }
-    return "Unknown"
-  },
-  landingPadName: function() {
-    var lp = Mart.Products.findOne(this.productId)
-    if(lp) {
-      return lp.name
-    }
-    return "Unknown"
+Template.reserveDocking.events({
+  "click .removeBooking": function(event, template) {
+    Mart.LineItem.remove(this._id)
   }
 });
