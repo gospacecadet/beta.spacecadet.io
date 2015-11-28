b41f524447bfd34a064d1c6f246c1cc8f8344c6a
Marvin Arnold <marvinmarnold@gmail.com>
2015-11-27 21:46:47 -0600

    show dockings other than current

--- client/templates/dockings/docking.js
+++ client/templates/dockings/docking.js
@@ -1,14 +1,28 @@
 Template.docking.onCreated(function() {
   var self = this
   self.autorun(function() {
-    var stationId = FlowRouter.getParam('stationId')
+    var dockingId = FlowRouter.getParam('dockingId')
     // Meteor.subscribe('mart/stations')
-    Meteor.subscribe("mart/carts", stationId);
-  });
-
-  self.autorun(function() {
-    var stationId = FlowRouter.getParam('stationId')
-    // Meteor.subscribe('mart/stations')
-    Meteor.subscribe("mart/products", stationId);
+    Meteor.subscribe("mart/cart", dockingId);
   });
 })
+
+Template.docking.helpers({
+  docking: function() {
+    var dockingId = FlowRouter.getParam('dockingId')
+    return Mart.Carts.findOne(dockingId)
+  },
+  dockings: function() {
+    return Mart.Cart.lineItems(FlowRouter.getParam('dockingId'))
+  },
+  // Link to station for current LI or home if not found
+  stationPath: function() {
+    var product = Mart.Products.findOne(this.productId)
+    if(product) {
+      var storefront = Mart.Storefronts.findOne(product.storefrontId)
+      if (storefront)
+        return FlowRouter.path("station", {stationId: storefront._id})
+    }
+    return flowRouterPath("homepage")
+  }
+});
