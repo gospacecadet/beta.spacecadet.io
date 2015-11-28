4177350e14680bd41f8edc9ead34a76905aad2af
Marvin Arnold <marvinmarnold@gmail.com>
2015-11-27 18:00:03 -0600

    Link to stations from cart page

--- client/templates/dockings/reserve-docking.js
+++ client/templates/dockings/reserve-docking.js
@@ -3,3 +3,16 @@ Template.reserveDocking.events({
     Mart.LineItem.remove(this._id)
   }
 });
+
+Template.reserveDocking.helpers({
+  // Link to station for current LI or home if not found
+  stationPath: function() {
+    var product = Mart.Products.findOne(this.productId)
+    if(product) {
+      var storefront = Mart.Storefronts.findOne(product.storefrontId)
+      if (storefront)
+        return FlowRouter.path("station", {stationId: storefront._id})
+    }
+    return flowRouterPath("homepage")
+  }
+});
