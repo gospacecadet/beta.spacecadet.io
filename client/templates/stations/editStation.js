16bf3e4b8642f894a1d1668dac2e632ecc0e03df
Marvin Arnold <marvinmarnold@gmail.com>
2015-12-31 23:54:36 -0600

    update station

--- client/templates/stations/editStation.js
+++ client/templates/stations/editStation.js
@@ -1,16 +1,24 @@
 Template.editStation.onCreated(function() {
-  // var thiz = this
-  // Tracker.autorun(function() {
-  //   if(thiz.subscriptionsReady() && Roles.userIsInRole(Meteor.user(), [
-  //       Mart.ROLES.GLOBAL.ADMIN,
-  //       Mart.ROLES.GLOBAL.MERCHANT,
-  //       Mart.ROLES.GLOBAL.REP,
-  //     ], Mart.ROLES.GROUPS.GLOBAL)) {
-  //       return
-  //   } else {
-  //     // FlowRouter.go('homepage')
-  //   }
-  // });
+    // var hooksObject = {
+    //   // Called when any submit operation succeeds
+    //   // insertDoc: The form input values in a document, suitable for use with insert(). This object has been cleaned and validated, but auto values and default values have not been added to it.
+    //   // updateDoc: The form input values in a modifier, suitable for use with update(). This object has not been validated.
+    //   // currentDoc: The object that's currently bound to the form through the doc attribute
+    //   after: {
+    //     update: function(error, result) {
+    //       console.log(result);
+    //       FlowRouter.go(FlowRouter.path('editStation', {stationId: result}))
+    //     }
+    //   },
+    //   onError: function(operation, error) {
+    //     if(error) {
+    //       console.log(error);
+    //       alert('Could not update')
+    //     }
+    //   }
+    // };
+    // AutoForm.addHooks(['insertStationForm'], hooksObject);
+
 
   Tracker.autorun(function() {
     var stationId = FlowRouter.getParam('stationId')
