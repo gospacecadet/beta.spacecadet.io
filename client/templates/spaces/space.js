069b84f1ea13b3a4b6250bbce90e65496670b309
Marvin Arnold <marvinmarnold@gmail.com>
2016-01-04 20:09:27 -0600

    unit selection buttons trigger unique actions

--- client/templates/spaces/space.js
+++ client/templates/spaces/space.js
@@ -11,6 +11,9 @@ Template.space.onCreated(function() {
   var productId = Template.currentData().spaceData._id
   Meteor.subscribe("mart/images/product", productId);
   Meteor.subscribe("mart/prices", productId);
+
+  // By default, see daily price
+  Session.setDefault(unitSession(this.propertyId), Mart.Product.UNITS.DAY)
 })
 
 Template.spaceTopUnitSelection.helpers({
@@ -24,6 +27,39 @@ Template.spaceTopUnitSelection.helpers({
     return unitPrice(this._id, Mart.Product.UNITS.MONTH)
   }
 });
+
+const UNIT_SESSION_PREFIX = "unit-selected-"
+Template.spaceTopUnitSelection.events({
+  "click #hourly-price": function(event, template) {
+     event.preventDefault()
+     Session.set(unitSession(this.propertyId), Mart.Product.UNITS.HOUR)
+  },
+  "click #daily-price": function(event, template) {
+     event.preventDefault()
+     Session.set(unitSession(this.propertyId), Mart.Product.UNITS.DAY)
+
+  },
+  "click #monthly-price": function(event, template) {
+     event.preventDefault()
+     Session.set(unitSession(this.propertyId), Mart.Product.UNITS.MONTH)
+  },
+});
+
+Template.spaceTopForUnit.helpers({
+  hourlySelected: function() {
+    return Session.get(unitSession(this.propertyId)) === Mart.Product.UNITS.HOUR
+  },
+  dailySelected: function() {
+    return Session.get(unitSession(this.propertyId)) === Mart.Product.UNITS.DAY
+  },
+  monthlySelected: function() {
+    return Session.get(unitSession(this.propertyId)) === Mart.Product.UNITS.MONTH
+  }
+});
+
+var unitSession = function(propertyId) {
+  return UNIT_SESSION_PREFIX + propertyId
+}
 
 var unitPrice = function(productId, unit) {
   var p = Mart.Prices.findOne({
