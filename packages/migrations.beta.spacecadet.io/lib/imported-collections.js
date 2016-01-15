805f82d1ab2d2c68efe4f0a1849b2c22b1759e2e
Marvin Arnold <marvinmarnold@gmail.com>
2016-01-14 20:24:22 -0600

    migrate within fiber

--- packages/migrations.beta.spacecadet.io/lib/imported-collections.js
+++ packages/migrations.beta.spacecadet.io/lib/imported-collections.js
@@ -1,7 +1,9 @@
-var importDB = new MongoInternals.RemoteCollectionDriver(Meteor.settings.IMPORT_MONGO_DB_URL);
+Meteor.startup(function(){
+  var importDB = new MongoInternals.RemoteCollectionDriver(Meteor.settings.IMPORT_MONGO_DB_URL);
 
-_.extend(Migrator, {
-  ImportedStations: new Mongo.Collection('stations', { _driver: importDB }),
-  ImportedUsers: new Mongo.Collection('backupUsers', { _driver: importDB }),
-  ImportedPads: new Mongo.Collection('pads', { _driver: importDB }),
-})
+  _.extend(Migrator, {
+    ImportedStations: new Mongo.Collection('stations', { _driver: importDB }),
+    ImportedUsers: new Mongo.Collection('backupUsers', { _driver: importDB }),
+    ImportedPads: new Mongo.Collection('pads', { _driver: importDB }),
+  })
+});
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,4 +1,5 @@
 Meteor.startup(function() {
+  Migrator.migrate()
   // Migrator.forceMigrate()
   // createTestAccounts()
 });
