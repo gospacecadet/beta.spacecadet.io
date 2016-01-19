aa42eeb18f913f25fb7008747f3423a6489dc2e6
Marvin Arnold <marvinmarnold@gmail.com>
2016-01-19 00:45:14 -0600

    Testing git upload script

--- git.sh
+++ git.sh
@@ -4,18 +4,18 @@ cd ~/workspace/spacecadet/beta.spacecadet.io
 git add .
 git commit -m "$1"
 git push origin master
-#
-# cd ~/workspace/spacecadet/mart-vibe-spacecadet
-# git add .
-# git commit -m $1
-# git push origin master
-#
-# cd ~/workspace/spacecadet/mart
-# git add .
-# git commit -m $1
-# git push origin master
-#
-# cd ~/workspace/spacecadet/bootstrap
-# git add .
-# git commit -m $1
-# git push origin spacecadet
+
+cd ~/workspace/spacecadet/mart-vibe-spacecadet
+git add .
+git commit -m "$1"
+git push origin master
+
+cd ~/workspace/spacecadet/mart
+git add .
+git commit -m "$1"
+git push origin master
+
+cd ~/workspace/spacecadet/bootstrap
+git add .
+git commit -m "$1"
+git push origin spacecadet
