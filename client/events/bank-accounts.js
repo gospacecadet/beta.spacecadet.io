4ccbcf81d7b541a27e3b61509e201df7eb6f645c
Marvin Arnold <marvinmarnold@gmail.com>
2016-01-20 13:40:55 -0600

    Can activate bank accounts on stripe

---
+++ client/events/bank-accounts.js
@@ -0,0 +1,12 @@
+Template.savedBankAccount.events({
+  "click .activate-bank-account-button": function(event, template){
+     Meteor.call('mart/stripe/activate-bank-account', template.data._id, function(error, result) {
+       if(error) {
+        //  console.log(error);
+         sAlert.error(error.message)
+       } else {
+         sAlert.success("Account successfully activated")
+       }
+     })
+  }
+});
