f6b8cd9cde2da435226b15bf227ab2272e0bb78e
Marvin Arnold <marvinmarnold@gmail.com>
2016-01-21 22:52:57 -0600

    Image upload on property insert

--- server/slingshot/property-directives.js
+++ server/slingshot/property-directives.js
@@ -5,28 +5,28 @@ var directives = [
 ]
 
 _.each(directives, function(directive) {
-  Slingshot.createDirective(directive.name, Slingshot.S3Storage, {
-    bucket: Meteor.settings.AWSBucket,
-    acl: "public-read",
+  Slingshot.createDirective(directive.name,
+    Slingshot.S3Storage, {
+      bucket: Meteor.settings.AWSBucket,
+      acl: "public-read",
+      authorize: function (file, metaContext) {
+        if (!Security.can(this.userId).insert({}).for(Mart.Storefronts).check()) {
+          var message = "Please login before posting files";
+          throw new Meteor.Error("Login Required", message);
+        }
 
-    authorize: function (file, metaContext) {
-      if (!Security.can(this.userId).insert({}).for(Mart.Storefronts).check()) {
-        var message = "Please login before posting files";
-        throw new Meteor.Error("Login Required", message);
+        return true;
+      },
+
+      key: function (file, metaContext) {
+        let extensions = file.name.split(".")
+        let extension = extensions[extensions.length-1]
+        return  this.userId +
+          "/properties" +
+          directive.path +
+          metaContext.index +
+          "." +
+          extension
       }
-
-      return true;
-    },
-
-    key: function (file, metaContext) {
-      let extensions = file.name.split(".")
-      let extension = extensions[extensions.length-1]
-      return "stations/" +
-        metaContext.objectId +
-        directive.path +
-        metaContext.index +
-        "." +
-        extension
-    }
-  });
+    });
 })
--- server/slingshot/space-directives.js
+++ server/slingshot/space-directives.js
@@ -1,32 +1,34 @@
-var directives = [
-  {name: "SpacesOriginal", path: "/original/"},
-  {name: "SpacesOptimized", path: "/optimized/"},
-  {name: "SpacesThumbnail", path: "/thumbnail/"},
-]
-
-_.each(directives, function(directive) {
-  Slingshot.createDirective(directive.name, Slingshot.S3Storage, {
-    bucket: Meteor.settings.AWSBucket,
-    acl: "public-read",
-
-    authorize: function (file, metaContext) {
-      if (!Security.can(this.userId).insert({}).for(Mart.Storefronts).check()) {
-        var message = "Please login before posting files";
-        throw new Meteor.Error("Login Required", message);
-      }
-
-      return true;
-    },
-
-    key: function (file, metaContext) {
-      let extensions = file.name.split(".")
-      let extension = extensions[extensions.length-1]
-      return "stations/" +
-        metaContext.objectId +
-        directive.path +
-        metaContext.index +
-        "." +
-        extension
-    }
-  });
-})
+// var directives = [
+//   {name: "SpacesOriginal", path: "/original/"},
+//   {name: "SpacesOptimized", path: "/optimized/"},
+//   {name: "SpacesThumbnail", path: "/thumbnail/"},
+// ]
+//
+// _.each(directives, function(directive) {
+//   Slingshot.createDirective(directive.name, Slingshot.S3Storage, {
+//     bucket: Meteor.settings.AWSBucket,
+//     acl: "public-read",
+//     pathPrefix: function(file) {
+//       return this.userId + "/spaces"
+//     },
+//     authorize: function (file, metaContext) {
+//       if(!Security.can(this.userId).
+//         insert({}).for(Mart.Storefronts).check()) {
+//
+//         var message = "Please login before posting files";
+//         throw new Meteor.Error("unauthorized-access", message);
+//       }
+//
+//       return true;
+//     },
+//
+//     key: function (file, metaContext) {
+//       let extensions = file.name.split(".")
+//       let extension = extensions[extensions.length-1]
+//       return  directive.path +
+//         metaContext.index +
+//         "." +
+//         extension
+//     }
+//   });
+// })
