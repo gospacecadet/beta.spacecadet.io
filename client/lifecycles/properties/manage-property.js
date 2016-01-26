69c704bfb9591c7354768410f150361a42ad5d59
Marvin Arnold <marvinmarnold@gmail.com>
2016-01-26 15:34:22 -0600

    Can edit and create properties and spaces

--- client/lifecycles/properties/manage-property.js
+++
@@ -1,7 +0,0 @@
-Template.manageProperty.onCreated(function() {
-  var template = this
-  Tracker.autorun(function(){
-    var propertyId = FlowRouter.getParam('propertyId')
-    template.subscribe("mart/storefront", propertyId); 
-  });
-})
