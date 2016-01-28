09a0371c94698cec26abca1ce26f182a1a4600d8
Marvin Arnold <marvinmarnold@gmail.com>
2016-01-27 21:52:03 -0600

    fixed multiple triggered signup issues

--- client/lifecycles/accounts/shopper-sign-up.js
+++ client/lifecycles/accounts/shopper-sign-up.js
@@ -1,29 +1,27 @@
-Template.shopperSignUp.onCreated(function() {
-  var hooksObject = {
-    onSubmit: function(insertDoc, updateDoc, currentDoc) {
-      var that = this
-      var shopper = {
-        email: insertDoc.email,
-        password: insertDoc.password,
-        roles: [Mart.ROLES.GLOBAL.SHOPPER],
-      }
-
-      Mart.Accounts.createUser(shopper, function(error) {
-        if(error) {
-          that.done(error)
-        } else {
-          $("#loginModal").modal('hide')
-          that.done()
-        }
-      })
-
-      return false;
-    },
-    onError: function(operation, error) {
-      if(error && error.reason) { // a special Meteor.error
-        sAlert.error(error.reason)
-      }
+var hooksObject = {
+  onSubmit: function(insertDoc, updateDoc, currentDoc) {
+    var that = this
+    var shopper = {
+      email: insertDoc.email,
+      password: insertDoc.password,
+      roles: [Mart.ROLES.GLOBAL.SHOPPER],
     }
-  };
-  AutoForm.addHooks(['shopper-sign-up-homepage'], hooksObject);
-})
+
+    Mart.Accounts.createUser(shopper, function(error) {
+      if(error) {
+        that.done(error)
+      } else {
+        $("#loginModal").modal('hide')
+        that.done()
+      }
+    })
+
+    return false;
+  },
+  onError: function(operation, error) {
+    if(error && error.reason) { // a special Meteor.error
+      sAlert.error(error.reason)
+    }
+  }
+};
+AutoForm.addHooks(['shopper-sign-up-homepage'], hooksObject);
