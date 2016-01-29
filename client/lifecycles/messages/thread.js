a6c14c72338e17dd4ecfb23aaad809b319c97c6b
Marvin Arnold <marvinmarnold@gmail.com>
2016-01-28 22:21:36 -0600

    xfer

--- client/lifecycles/messages/thread.js
+++ client/lifecycles/messages/thread.js
@@ -1,4 +1,8 @@
 Template.thread.onCreated(function() {
-  Meteor.subscribe("talk/messages", Template.currentData()._id);
-  Meteor.subscribe("mart/profile", Template.currentData().recipientId());
+  this.subscribe("talk/messages", Template.currentData()._id);
+  this.subscribe("mart/profile", Template.currentData().recipientId());
 })
+//
+// Template.threadAttachFile.onCreated(function() {
+//   Session.setDefault("threadFile")
+// })
