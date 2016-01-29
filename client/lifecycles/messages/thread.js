8514fea52a85f3bebc41f59143340699b990cc76
Marvin Arnold <marvinmarnold@gmail.com>
2016-01-28 22:34:28 -0600

    Can attach files to threads

--- client/lifecycles/messages/thread.js
+++ client/lifecycles/messages/thread.js
@@ -3,6 +3,6 @@ Template.thread.onCreated(function() {
   this.subscribe("mart/profile", Template.currentData().recipientId());
 })
 //
-// Template.threadAttachFile.onCreated(function() {
-//   Session.setDefault("threadFile")
-// })
+Template.threadModal.onCreated(function() {
+  this.subscribe("talk/messages", Template.currentData()._id);
+})
