d1391e782f23169cc54afa2db467ad2ac1d1d480
Marvin Arnold <marvinmarnold@gmail.com>
2016-01-29 21:41:54 -0600

    Clean threads script

---
+++ server/cron/clean-threads.js
@@ -0,0 +1,22 @@
+SyncedCron.add({
+  name: 'Remove threads that dont have any messages due to hack in messaging logic',
+  schedule: function(parser) {
+    // parser is a later.parse object
+    return parser.text('every 1 hours');
+  },
+  job: function() {
+    var threads = Talk.Threads.find().fetch()
+
+    _.each(threads, function(thread) {
+      var oneMessage = Talk.Messages.findOne({threadId: thread._id})
+
+      if(!oneMessage) {
+        Talk.Threads.remove(thread._id)
+      }
+    })
+  }
+});
+
+Meteor.startup(function(){
+  SyncedCron.start();
+});
