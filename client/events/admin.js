dcea8ea75a8745ad2b1a11e907d131c4d53addcc
Marvin Arnold <marvinmarnold@gmail.com>
2016-03-19 19:31:18 -0500

    reject xfer

--- client/events/admin.js
+++ client/events/admin.js
@@ -1,8 +1,10 @@
 Template.transferFundsApproval.events({
   "click .accept-transfer-button": function(event, template) {
+    event.target.disabled = true
     Meteor.call("mart/process-transfer", template.data._id, function(error, result) {
       if(error) {
-        console.log(error);
+        // console.log(error);
+        event.target.disabled = false
         sAlert.error(error)
       } else {
         sAlert.success("Transfer initiated. It may take up to 3 days to show up on Stripe.")
@@ -10,9 +12,11 @@ Template.transferFundsApproval.events({
     })
   },
   "click .reject-transfer-button": function(event, template) {
+    event.target.disabled = true
     Meteor.call("mart/reject-transfer", template.data._id, function(error, result) {
       if(error) {
-        console.log(error);
+        event.target.disabled = false
+        // console.log(error);
         sAlert.error(error)
       } else {
         sAlert.success("Transfer rejected")
