7795b1cf1210ae750757306688607572c8841c04
Marvin Arnold <marvinmarnold@gmail.com>
2016-06-03 16:52:47 -0500

    Activate Subscriptions at the right time

--- server/cron/process-subscriptions.js
+++ server/cron/process-subscriptions.js
@@ -1,15 +1,16 @@
 SyncedCron.add({
   name: 'Process subscriptions that are due for payment',
-  schedule: function(parser) {
+  schedule(parser) {
     // parser is a later.parse object
     return parser.text('every 1 days');
   },
-  job: function() {
+  job() {
     var subscriptions = Mart.Subscriptions.find({
       state:  Mart.Subscription.STATES.SPAWNED
     }).fetch()
 
-    _.each(subscriptions, function(subscription) {
+    _.each(subscriptions, subscription => {
+      subscription.activate()
       subscription.process()
     })
   }
